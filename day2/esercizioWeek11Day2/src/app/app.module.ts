a9c3297a1299af6f0608ca30bfac6e182a870814
Walter Barbieri <128515233+uolli90@users.noreply.github.com>
2023-06-06 17:37:51 +0200

    WalterWeek11Day2Esercizio - Versione Reactive manca Select funzionante e campi dinamici

--- day2/esercizioWeek11Day2/src/app/app.module.ts
+++ day2/esercizioWeek11Day2/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TemplateComponent } from './components/template/template.component';
+import { ReactiveComponent } from './components/reactive/reactive.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateComponent
+    TemplateComponent,
+    ReactiveComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ day2/esercizioWeek11Day2/src/app/components/reactive/reactive.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validator, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive',
+  templateUrl: './reactive.component.html',
+  styleUrls: ['./reactive.component.scss']
+})
+export class ReactiveComponent implements OnInit {
+
+  heroForm!: FormGroup;
+
+  hero: any = {
+    name: '',
+    alterEgo: '',
+    power: '',
+    enemy: '',
+    planet: '',
+    weakness: ''
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.heroForm = new FormGroup ({
+      name: new FormControl(null, Validators.required),
+      alterEgo: new FormControl(null, Validators.required),
+      power: new FormControl(null, Validators.required),
+      enemy: new FormControl(null, Validators.maxLength(10)),
+      planet: new FormControl(null, [Validators.required, Validators.minLength(5)])
+    })
+
+    this.heroForm.statusChanges?.subscribe(stato => {
+      console.log(this.heroForm);
+      console.log(stato);
+    })
+  }
+
+  submitForm() {
+    console.log('Form inviato: ', this.heroForm);
+    this.hero.name = this.heroForm.value.name;
+    this.hero.alterEgo = this.heroForm.value.alterEgo;
+    this.hero.power = this.heroForm.value.power;
+    this.hero.enemy = this.heroForm.value.enemy;
+    this.hero.planet = this.heroForm.value.planet;
+    this.hero.weakness = this.heroForm.value.weakness;
+    this.heroForm.reset();
+  }
+
+}
